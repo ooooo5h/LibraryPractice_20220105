f75f0644da3f279a5bf1f455f0072d8e8fd4a125
ooooo5h <ooooo5h91@gmail.com>
2022-01-05 10:56:35 +0900

    Glide 라이브러리 설치 / 코틀린 코드 작성했는데 이미지뷰가 안불러와짐

--- app/src/main/java/com/neppplus/librarypractice_20220105/MainActivity.kt
+++ app/src/main/java/com/neppplus/librarypractice_20220105/MainActivity.kt
@@ -3,6 +3,7 @@ package com.neppplus.librarypractice_20220105
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -17,6 +18,9 @@ class MainActivity : AppCompatActivity() {
             startActivity(myIntent)
 
         }
+
+//        Glide 이용해서 웹의 이미지를 -> imgRecent에 반영하기
+        Glide.with(this).load("https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.joongang.co.kr%2Farticle%2F25008728&psig=AOvVaw0uzGDRNdyrb117bHS6JTk9&ust=1641433192226000&source=images&cd=vfe&ved=0CAsQjRxqFwoTCPiRuqW9mfUCFQAAAAAdAAAAABAJ").into(imgRecent)
 
     }
 }
