0419bc26526d9bd2b7171ac119d2ff854db99ce9
ooooo5h <ooooo5h91@gmail.com>
2022-01-05 14:56:28 +0900

    Handler / postDelayed 함수   - 일정시간 지난 후에 기능 실행

--- app/src/main/java/com/neppplus/librarypractice_20220105/SplashActivity.kt
+++ app/src/main/java/com/neppplus/librarypractice_20220105/SplashActivity.kt
@@ -3,16 +3,30 @@ package com.neppplus.librarypractice_20220105
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        val myIntent = Intent(this, MainActivity::class.java)
-        startActivity(myIntent)
+
+//         2.5초간 대기 후에 화면이동 & 로딩화면 종료 처리를 하자
+        val myHandler = Handler(Looper.getMainLooper())
+
+//        2500 ms 후에 { }안에 있는 코드를 실행 (작업 지연/실행)
+        myHandler.postDelayed({
+
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
 
 //        메인화면으로 넘어가고 나면, 이 화면 종료
-        finish()
+            finish()
+
+        }, 2500)
+
+
+
     }
 }
